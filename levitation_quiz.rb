01a7527cb71949f25ccaa0678154f33007cf75f0
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-15 07:57:12 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,11 +1,11 @@
 
 def levitation_quiz
   while true
+    puts "What is the spell that enacts levitation?"
     input = gets.chop
     break if input == "Wingardium Leviosa"
-    
-  
   end
+  puts ""
 end
 
 
